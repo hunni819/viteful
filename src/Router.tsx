3d7017c8a4b5bb40bcb84faa12a594ddbc287cde
hun <pursuit0819@naver.com>
2025-01-24 10:46:30 +0900

    filebase routing

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,14 +1,58 @@
 import { Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
-import ProfilePage from './pages/ProfilePage';
-import HomePage from './pages/HomePages';
+
+type CustomElementType = () => JSX.Element;
+
+type CustomModule = {
+  default: CustomElementType;
+};
+
+type CustomRoute = {
+  path: string;
+  Element: CustomElementType;
+};
+
+const pageFiles = import.meta.glob('./pages/**/*.tsx', { eager: true });
+const tempRoutes: CustomRoute[] = [];
+
+for (const filePath of Object.keys(pageFiles)) {
+  try {
+    const fileName = filePath.match(/\.\/pages\/(.*)\.tsx$/)?.[1] ?? '';
+    // "./pages/*.tsx" -> "**"
+    if (!fileName) continue;
+    // "*" -> "/"
+    // "./pages/blog/$id.tsx" -> "/blog/:id"
+    const normalizedPathName = fileName.includes('&')
+      ? fileName.replace('&', ':')
+      : fileName.replace(/\/index/, '');
+
+    tempRoutes.push({
+      path:
+        fileName === 'index'
+          ? '/'
+          : `/${normalizedPathName.toLocaleLowerCase()}`,
+      Element: (pageFiles[filePath] as CustomModule).default,
+    });
+  } catch (err) {
+    console.error(err);
+    continue;
+  }
+}
+
+console.log(tempRoutes);
+
+const routes = tempRoutes.filter(
+  (routes, index, arr) =>
+    arr.findIndex((_route) => _route.path === routes.path) === index
+);
 
 const Router = () => {
   return (
     <Layout>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/profile" element={<ProfilePage />} />
+        {routes.map(({ path, Element }) => (
+          <Route key={`route-key-${path}`} path={path} element={<Element />} />
+        ))}
       </Routes>
     </Layout>
   );
